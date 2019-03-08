24c3ffca26caeea90b360dc38d95648c505b6d7d
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-03-08 14:53:04 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312576Whilestringexample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string myWord = "Bonjour mon ami";
+            string output = "";
+            while(myWord.Length > 0)
+            {
+                output += myWord.Substring(0, 1) + Environment.NewLine;
+                myWord = myWord.Substring(1);
+
+            }
+            lblOutput.Content = output;
+        }
+    }
+}
